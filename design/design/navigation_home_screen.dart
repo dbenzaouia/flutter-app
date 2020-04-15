eff0c09c5e7e1e92327b6d8ca6c7cec9cc6aec71
hlarchi <43379288+hlarchi@users.noreply.github.com>
2020-04-15 10:34:43 +0200

    Add files via upload
    correction du chemin principale

---
+++ design/design/navigation_home_screen.dart
@@ -0,0 +1,66 @@
+import './app_theme.dart';
+import './custom_drawer/drawer_user_controller.dart';
+import './custom_drawer/home_drawer.dart';
+import './setting_screen.dart';
+import './home_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget screenView;
+  DrawerIndex drawerIndex;
+  AnimationController sliderAnimationController;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            animationController: (AnimationController animationController) {
+              sliderAnimationController = animationController;
+            },
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+            },
+            screenView: screenView,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      if (drawerIndex == DrawerIndex.HOME) {
+        setState(() {
+          screenView = const MyHomePage();
+        });
+      } else if (drawerIndex == DrawerIndex.Config) {
+        setState(() {
+          screenView = ConfigScreen();
+        });
+      } else {
+        //do in your way......
+      }
+    }
+  }
+}
---
+++ design/design/ui_view/workout_view.dart
@@ -0,0 +1,155 @@
+import '../../main.dart';
+import 'package:flutter/material.dart';
+import '../second_app_theme.dart';
+
+class WorkoutView extends StatelessWidget {
+  final AnimationController animationController;
+  final Animation animation;
+
+  const WorkoutView({Key key, this.animationController, this.animation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (BuildContext context, Widget child) {
+        return FadeTransition(
+          opacity: animation,
+          child: new Transform(
+            transform: new Matrix4.translationValues(
+                0.0, 30 * (1.0 - animation.value), 0.0),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 24, right: 24, top: 16, bottom: 18),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    SecondAppTheme.nearlyDarkBlue,
+                    HexColor("#6F56E8")
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8.0),
+                      bottomLeft: Radius.circular(8.0),
+                      bottomRight: Radius.circular(8.0),
+                      topRight: Radius.circular(68.0)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: SecondAppTheme.grey.withOpacity(0.6),
+                        offset: Offset(1.1, 1.1),
+                        blurRadius: 10.0),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Aujourd hui',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontFamily: SecondAppTheme.fontName,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                          letterSpacing: 0.0,
+                          color: SecondAppTheme.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          'Vous avez effectuez :',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontFamily: SecondAppTheme.fontName,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20,
+                            letterSpacing: 0.0,
+                            color: SecondAppTheme.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4),
+                              child: Icon(
+                                Icons.timer,
+                                color: SecondAppTheme.white,
+                                size: 20,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4.0),
+                              child: Text(
+                                '68 min',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: SecondAppTheme.fontName,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  letterSpacing: 0.0,
+                                  color: SecondAppTheme.white,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4),
+                              child: Icon(
+                                Icons.directions_run,
+                                color: SecondAppTheme.white,
+                                size: 20,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 4.0,top: 8.0),
+                              child: Text(
+                                '7256 pas',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: SecondAppTheme.fontName,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  letterSpacing: 0.0,
+                                  color: SecondAppTheme.white,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: SizedBox(),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
