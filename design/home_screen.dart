2d31b08c14b8c8ad4d1b2cfa9ad8220f1cbcef88
hlarchi <43379288+hlarchi@users.noreply.github.com>
2020-04-15 10:27:02 +0200

    Add files via upload
    intégration du design avec l'app principale

---
+++ design/home_screen.dart
@@ -0,0 +1,117 @@
+import 'package:design/design/app_theme.dart';
+import 'overview_screen.dart';
+import 'second_app_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController animationController;
+  bool multiple = true;
+  Widget tabBody = Container(
+    color: SecondAppTheme.background,
+  );
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+    tabBody = MyDiaryScreen(animationController: animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.white,
+      body: FutureBuilder<bool>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (!snapshot.hasData) {
+            return const SizedBox();
+          } else {
+            return Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  appBar(),
+                  Expanded(
+                    child: FutureBuilder<bool>(
+                      future: getData(),
+                      builder:
+                          (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                        if (!snapshot.hasData) {
+                          return const SizedBox();
+                        } else {
+                          return Stack(
+                            children: <Widget>[
+                              tabBody,
+                            ],
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 200));
+    return true;
+  }
+
+  Widget appBar() {
+    return SizedBox(
+      height: AppBar().preferredSize.height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8, left: 8),
+            child: Container(
+              width: AppBar().preferredSize.height - 8,
+              height: AppBar().preferredSize.height - 8,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: Text(
+                  'Flutter App',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: AppTheme.darkText,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8, right: 40),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ design/setting_screen.dart
@@ -0,0 +1,43 @@
+import 'package:design/design/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'second_app_theme.dart';
+import 'package:design/Configuration.dart';
+
+
+class ConfigScreen extends StatefulWidget {
+  @override
+  _ConfigScreenState createState() => _ConfigScreenState();
+}
+
+class _ConfigScreenState extends State<ConfigScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Setting",
+          style: TextStyle(
+            fontSize: 22,
+            color: AppTheme.darkText,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        //margin: const EdgeInsets.only(left: 4.0, right: 4.0, bottom: 300.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Configuration(),
+          ],
+        ),
+      ),
+    );
+  }
+}
